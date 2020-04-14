330f3898686fe2abd8136e4e94c3cd9677c0bd6d
Timo Schrader <timo@schraderonline.net>
2020-04-14 19:20:37 +0200

    Implemented MainApp in ScreenSelector

--- frontend/src/ScreenSelector/ScreenSelector.js
+++ frontend/src/ScreenSelector/ScreenSelector.js
@@ -4,28 +4,40 @@ import axios from 'axios';
 import config from './../config.json';
 
 import LoginScreen from './screens/LoginScreen';
+import MainApp from './screens/MainApp';
 
 class ScreenSelector extends React.Component {
-    componentDidMount() {
-        let request = axios.get(config.server + 'api/sessionapi/loginState.php')
+    constructor(props) {
+        super(props)
 
         this.completeLoginCheck = this.completeLoginCheck.bind(this)
+        this.handleScreenChangeEvent = this.handleScreenChangeEvent.bind(this)
+    }
+    
+    componentDidMount() {
+        
+        document.addEventListener('screenChange',this.handleScreenChangeEvent)
 
+        let request = axios.get(config.server + 'api/sessionapi/loginState.php',{ withCredentials: true })
         request.then(this.completeLoginCheck)
+    }
+
+    handleScreenChangeEvent(e) {
+        this.setState({currentScreen : e.detail.newScreen})
     }
 
     completeLoginCheck(response) {
         if(response.data.loginState) {
-            
+            this.setState({currentScreen : 'mainApp'})
         }
         else
         {
-            this.setState({'currentScreen' : 'login'})
+            this.setState({currentScreen : 'login'})
         }
     }
 
     state = {
-        'currentScreen' : 'loading'
+        currentScreen : 'loading'
     }
     
     render() {
@@ -38,6 +50,10 @@ class ScreenSelector extends React.Component {
             case 'login':
                 return(
                     <LoginScreen />
+                )
+            case 'mainApp':
+                return(
+                    <MainApp />
                 )
             default:
                 return(
