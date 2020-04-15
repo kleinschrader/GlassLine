55194e2a52c572a424ff28f11ca3caa169dd7eec
Timo Schrader <timo@schraderonline.net>
2020-04-15 23:08:09 +0200

    Updated the Uuidfrombin function to return string

--- database/schema.sql
+++ database/schema.sql
@@ -33,16 +33,16 @@ RETURN
         SUBSTR(_uuid, 25) ));
 //
 CREATE FUNCTION UuidFromBin(_bin BINARY(16))
-    RETURNS BINARY(36)
+    RETURNS CHAR(36)
     LANGUAGE SQL  DETERMINISTIC  CONTAINS SQL  SQL SECURITY INVOKER
 RETURN
-    LCASE(CONCAT_WS('-',
+    CAST(LCASE(CONCAT_WS('-',
         HEX(SUBSTR(_bin,  5, 4)),
         HEX(SUBSTR(_bin,  3, 2)),
         HEX(SUBSTR(_bin,  1, 2)),
         HEX(SUBSTR(_bin,  9, 2)),
         HEX(SUBSTR(_bin, 11))
-              ));
+              )) AS CHAR);
 
 //
 DELIMITER ;
@@ -56,7 +56,7 @@ VALUES
   (
     UuidToBin(UUID()),
     "administrator",
-    "$2y$10$5WdMBALndcf9j.yaWupUte4Xce5o7Qt9OwejVbBu6JqBFY.1.EmLi",
+    "$2b$10$NEnpthiO6Iu2rdnp5FkBWuXNfyDV4Rx9BXFVlNr28hw07t99GK0eK",
     true
   );
 
