2397b318bd9757fba6f1ecb6500efcef15688d0a
Timo Schrader <timo@schraderonline.net>
2020-04-19 21:36:28 +0200

    Implement Serverlist

--- frontend/src/ScreenSelector/screens/MainApp/SubScreen/ServerSubS/ServerSubS.js
+++ frontend/src/ScreenSelector/screens/MainApp/SubScreen/ServerSubS/ServerSubS.js
@@ -8,11 +8,15 @@ class ServerSubS extends React.Component {
         this.handleTenantCallback = this.handleTenantCallback.bind(this)
         this.handleTenantChange = this.handleTenantChange.bind(this)
         this.handleServerCallback = this.handleServerCallback.bind(this)
+        this.handleServerListCallback = this.handleServerListCallback.bind(this)
     }
 
     componentDidMount() {
-        let request = document.WSClient.getTenants()
-        request.addEventListener('complete',this.handleTenantCallback)
+        let tenantRequest = document.WSClient.getTenants()
+        tenantRequest.addEventListener('complete',this.handleTenantCallback)
+
+        let serverlistRequest = document.WSClient.getAllServers()
+        serverlistRequest.addEventListener('complete',this.handleServerListCallback)
     }
     
     handleTenantCallback(r) {
@@ -33,6 +37,10 @@ class ServerSubS extends React.Component {
         this.setState({parents : r.detail.data.server})
     }
 
+    handleServerListCallback(r) {
+        this.setState({serverList : r.detail.data.server})
+    }
+
     handleSubmit (e) {
         e.preventDefault();
 
@@ -44,6 +52,8 @@ class ServerSubS extends React.Component {
     state = {
         tenants : [],
         parents : [],
+        serverList : [],
+        adminTenant : false,
         setTenant : ""
     }
 
@@ -54,6 +64,12 @@ class ServerSubS extends React.Component {
         const serverOptions = this.state.parents.map((elem) => 
         <option key={elem.id} value={elem.id}>{elem.name}</option>
         );
+        const serverList = this.state.serverList.map((elem) => 
+        <tr>
+            <td>{elem.servername}</td>
+            <td>{elem.tenantname}</td>
+        </tr>
+        );
         return (
             <div className="ServerSubS SubScreen">
                 <form className="CreateServerForm" onSubmit={this.handleSubmit}>
@@ -62,14 +78,14 @@ class ServerSubS extends React.Component {
                     <input maxLength="64" id="input_servername" name="servername"></input>
                     <div className="DualInputSplitter">
                         <div>
-                            <label for="input_tenant">Tenant</label>
+                            <label htmlFor="input_tenant">Tenant</label>
                             <select id="input_tenant" defaultValue="NONE" name="tenant" onChange={this.handleTenantChange}>
                                 <option value="NONE" hidden disabled >Select Tenant</option>
                                 {tenantOptions}
                             </select>
                         </div>
                         <div>
-                            <label for="input_parent">Parent Server</label>
+                            <label htmlFor="input_parent">Parent Server</label>
                             <select id="input_parent" defaultValue="00000000-0000-0000-0000-000000000000" name="parent" disabled={this.state.setTenant === ""}>
                                 <option value="00000000-0000-0000-0000-000000000000" >Kein Parent Server</option>
                                 {serverOptions}
@@ -78,6 +94,21 @@ class ServerSubS extends React.Component {
                     </div>
                     <input type="submit" value="Create Server"/>
                 </form>
+                <div className="ServerListBlock">
+                    <h3>Serverlist</h3>
+                    <label htmlFor="input_serverlisttenant" >Filter by Tenant</label>
+                    <select id="input_serverlisttenant" defaultValue="00000000-0000-0000-0000-000000000000">
+                        <option value="00000000-0000-0000-0000-000000000000"> Dont Filter</option>
+                        {tenantOptions}
+                    </select>
+                    <table className="ServerListTable">
+                        <tr>
+                            <th>Servername</th>
+                            <th>Tenant</th>
+                        </tr>
+                        {serverList}
+                    </table>
+                </div>
             </div>
         )
     }
