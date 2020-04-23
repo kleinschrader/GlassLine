dc2e9095a2ab04712ae196f6184d6e2f0efab2f7
Timo Schrader <timo@schraderonline.net>
2020-04-23 22:27:56 +0200

    Removed PHP backend

--- backend/source/mysql.php
+++
@@ -1,7 +0,0 @@
-<?php
-    define('DBUSER','db_user_gl');
-    define('DBPWD','passwd12');
-    define('DBHOST','database');
-    define('DBNAME','glassline');
-    $db = new mysqli(DBHOST,DBUSER,DBPWD,DBNAME);
-?>
