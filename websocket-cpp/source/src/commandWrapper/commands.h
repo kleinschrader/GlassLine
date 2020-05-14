3a38eb870610f8921fa2776bdb1b7e9863d3a848
Timo Schrader [TechnoSoft Consulting GmbH] <tschrader@tcg-online.de>
2020-05-14 07:49:32 +0200

    Added a few comments

--- websocket-cpp/source/src/commandWrapper/commands.h
+++ websocket-cpp/source/src/commandWrapper/commands.h
@@ -1,6 +1,8 @@
 #ifndef commands_H_GUARD
 #define commands_H_GUARD
 
+//this file is just ment to be a shortcut to include all command Classes
+
 #include "commands/getSetupRequired/getSetupRequired.h"
 #include "commands/checkSetupToken/checkSetupToken.h"
 #include "commands/checkTokenLogin/checkTokenLogin.h"
--- websocket-cpp/source/src/commandWrapper/commands/checkSetupToken/checkSetupToken.h
+++ websocket-cpp/source/src/commandWrapper/commands/checkSetupToken/checkSetupToken.h
@@ -8,5 +8,10 @@ public:
     checkSetupToken() {};
     ~checkSetupToken() {};
 
+    /**
+     * Checks if a user with a setup token exists and log him on
+     *
+     * @param args["token"] A 128 Bit token
+    */
     void run(const nlohmann::json &args);
 };
